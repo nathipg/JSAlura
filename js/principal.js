41d8570ca8e8a7efb342fd8651251a26980d8133
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-01-29 15:23:41 -0300

    Aula 3

--- js/principal.js
+++ js/principal.js
@@ -6,14 +6,17 @@ document.querySelectorAll('#tabela-pacientes tr').forEach(paciente => {
     const altura = paciente.querySelector('.info-altura').textContent;
 
     if(peso <= 0 || peso > 1000) {
+        paciente.classList.add('invalido');
         paciente.querySelector('.info-imc').textContent = 'Peso inválido';
         return false;
     }
 
     if(altura <= 0 || altura > 3) {
+        paciente.classList.add('invalido');
         paciente.querySelector('.info-imc').textContent = 'Altura inválida';
         return false;
     }
 
-    paciente.querySelector('.info-imc').textContent = (peso / (altura * altura)).toFixed(2);
+    const imc = peso / (altura * altura);
+    paciente.querySelector('.info-imc').textContent = imc.toFixed(2);
 });
